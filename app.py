e701cb59bfb8978fc562e47098f93441035a994e
Pratham bhardwaj <87937949+prathambhardwaj0307@users.noreply.github.com>
2022-06-25 16:52:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("./templates/index.html")
 
 
 @app.route('/prediction', methods=['GET', 'POST'])
